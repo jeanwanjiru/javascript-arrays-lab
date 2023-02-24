f1a946520030f80e00610693224a24eadfb55d70
jeanwanjiru <jeanwanjiru06@gmail.com>
2023-02-24 13:34:10 -0500

    waah

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-var kittens = [] //define your array here
+  var  kittens = ["Milo","Otis","Garfield"] 
 
-// Add your functions and code here
+
+ function destructivelyAppendKitten (name){
+    kittens.push(name);
+ }
+function destructivelyPrependKitten(name){
+    kittens.unshift(name);
+}
+function destructivelyRemoveFirstKitten(name){
+    kittens.shift(name);
+}
+function destructivelyRemoveLastKitten(name){
+    kittens.pop(name);
+}
+function appendKitten(name){
+   return kittens.concat(name);
+}
+    function prependKitten(name){
+          const newkittens = [name, ...kittens];
+          return newkittens;
+    }function removeLastKitten(){
+        var caats= kittens.slice(0,kittens.length-1);
+        return caats;
+      }
+
+function removeFirstKitten(){
+  var catts= kittens.slice(1)
+  return catts
+}
+    
+    
